1009cf64d393679de3c84c18d83b769bee60c90b
HashLizard <150775819+HashLizard@users.noreply.github.com>
2023-11-14 14:55:51 +0800

    Update update_pip.sh

--- update_pip.sh
+++ update_pip.sh
@@ -9,8 +9,8 @@ tmp_dir=$(mktemp -d -t ddsp-XXXX)
 git clone https://github.com/magenta/ddsp.git $tmp_dir
 cd $tmp_dir
 
-python setup.py sdist
-python setup.py bdist_wheel --universal
+python3 setup.py sdist
+python3 setup.py bdist_wheel --universal
 twine upload dist/*
 
 cd $orig_dir
